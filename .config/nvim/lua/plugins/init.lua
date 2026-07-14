41e4b340d75f7a0946fb52aab23ada69b2b3fd02
al <shabanajshaban92@gmail.com>
2026-07-14 09:14:02 +0200

    update nvim config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,44 +1,34 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-{
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "cpp",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "lua",
-        "bash",
-        "fish",
-        "zsh",
-        "tsx",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter",
+    config = function()
+      require("nvim-treesitter").install({
+        "c", "cpp", "python", "javascript", "typescript",
+        "html", "css", "lua", "bash",
+        "fish", "zsh", "tsx",
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "c", "cpp", "python", "javascript", "typescript",
+          "html", "css", "lua", "bash",
+          "sh", "zsh", "fish", "tsx", "python",
+        },
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
+  },
 }
