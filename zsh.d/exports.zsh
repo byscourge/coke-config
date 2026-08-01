c92d5bdd7672d8ebff3409b11805cd306c99e0ab
al <shabanajshaban92@gmail.com>
2026-08-01 16:30:19 +0200

    cleanups

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -1,19 +1,22 @@
 HISTFILE="$HOME/.zsh_history"
 HISTSIZE=50000
 SAVEHIST=50000
+
 bindkey -e
+
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt INTERACTIVE_COMMENTS
+
 export pkg
-export PREFIX="/data/data/com.termux/files/usr" #most termux bins usually use prefix plus its handy
-export PATH="$PREFIX/bin:$HOME/.local/bin" ## cleaned up and removed :PATH so it doesnt show like 5 fucking instances of the same paths, if i do need another path i can just add it here.
+export PREFIX="/data/data/com.termux/files/usr"
+export PATH="$PREFIX/bin:$HOME/.local/bin"
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export TERM=xterm-256color ## pretty colors
+export TERM=xterm-256color
 export EDITOR=nvim
 export COLUMNS LINES
 export bin="$PREFIX/bin/"
