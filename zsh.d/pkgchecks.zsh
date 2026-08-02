9d249e4066cd69ec550534eaab5d2965ca46908f
al <shabanajshaban92@gmail.com>
2026-08-02 07:47:31 +0200

    improve pkgchecks validation & corruption safety

--- zsh.d/pkgchecks.zsh
+++ zsh.d/pkgchecks.zsh
@@ -5,61 +5,98 @@ fi
 local __echoToPkg() {
   echo "$1" > ~/.pkg
 }
+
 alias repkg='rm ~/.pkg && szsh'
 alias szsh='clear && source ~/.zshrc'
 
 if [[ ! -s ~/.pkg ]]; then
-  warn "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nif you input something incorrectly, you can run the command"
-  c_info " repkg "
-  warn "to run this prompt again."
-  w_info "\n\nif you're unsure which package manager you have and you have not modified it, then type in"
-  c_info " default "
-  w_info "to choose the default package manager that termux comes with."
-  info "\n\ninput your package manager. [default / apt / pacman]: "
-  read zzz
-  case "$zzz" in
-    Apt | apt | APT)
-      __echoToPkg apt
-      pkg="apt"
-      clear
-      bc_info "APT was chosen as the primary package manager.\n"
-      return 0
-      ;;
-    Pacman | pacman | PACMAN)
-      __echoToPkg pacman
-      pkg="pacman"
-      clear
-      bc_info "PACMAN was chosen as the primary package manager.\n"
-      return 0
-      ;;
-    Default | default | DEFAULT)
-      __echoToPkg apt
-      pkg="apt"
-      clear
-      bc_info "APT was chosen as the primary package manager.\n"
-      return 0
-      ;;
-    *)
-      critical "\ncritical: BAD input given, retry"
-      sleep 0.6
-      critical "."
-      sleep 0.6
-      critical "."
-      sleep 0.6
-      critical "."
-      repkg
-      ;;
-  esac
+      if [[ -f $PREFIX/bin/apt && -f $PREFIX/bin/pacman ]]; then
+        echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+        err "Unable to set package manager as you have two (apt + pacman).\n"
+        info "Choose your primary [ apt / pacman ]: "
+        local zzz
+        read -r zzz
+        case "$zzz" in
+           Apt | apt | APT)
+             __echoToPkg apt
+             pkg="apt"
+             clear
+             bc_info "APT was set as the primary package manager.\n"
+             return 0
+             ;;
+ 
+           Pacman | pacman | PACMAN)
+             __echoToPkg pacman
+             pkg="pacman"
+             clear
+             bc_info "PACMAN was set as the primary package manager.\n"
+             return 0
+             ;;
+
+           *)
+             err "Invalid prompt!"
+             sleep 1
+             repkg
+             ;;
+         esac
+      fi
+
+      if [[ ! -f $PREFIX/bin/apt && ! -f $PREFIX/bin/pacman ]]; then
+        err "Unable to set package manager as no supported were detected.\n"
+        rm ~/.pkg
+        return
+      fi
+
+      if [[ -f $PREFIX/bin/apt ]] && apt --version &>/dev/null; then
+        __echoToPkg apt
+        pkg="apt"
+        clear
+        bc_info "APT was set as the primary package manager.\n"
+        return 0
+      fi
+
+      if [[ -f $PREFIX/bin/pacman ]] && pacman -V &>/dev/null; then
+        __echoToPkg pacman
+        pkg="pacman"
+        clear
+        bc_info "PACMAN was set as the primary package manager.\n"
+        return 0
+      fi
 fi
 
-if [[ "$(cat ~/.pkg)" != "apt" && "$(cat ~/.pkg)" != "pacman" ]]; then
+filevalid="$(cat ~/.pkg)"
+
+
+
+
+
+if [[ "$filevalid" != "apt" && "$filevalid" != "pacman" ]]; then
   clear
-  for i in {1..40}; do     
+  for i in {1..30}; do     
     err "Invalid package variable detected!\n"
     sleep 0.005
   done
-  sleep 0.5
   repkg
+fi
+
+if [[ "$filevalid" == "apt" ]]; then
+  if [[ ! -f $PREFIX/bin/apt ]]; then
+    for i in {1..30}; do     
+      err "Invalid package variable detected!\n"
+      sleep 0.005
+    done
+    repkg
+  fi
+fi
+
+if [[ "$filevalid" == "pacman" ]]; then
+  if [[ ! -f $PREFIX/bin/pacman ]]; then
+    for i in {1..30}; do     
+      err "Invalid package variable detected!\n"
+      sleep 0.005
+    done
+    repkg
+  fi
 fi
 
 declare -g pkg=$(cat ~/.pkg)
