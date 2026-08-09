1cb1663c05ac1d10acbe27bb896fba36fd8ceaee
al <shabanajshaban92@gmail.com>
2026-08-09 11:00:18 +0200

    integrate pkgchecks, add portable functions (runsilent, pkgcheck API)

--- zsh.d/pkgchecks.zsh
+++ zsh.d/pkgchecks.zsh
@@ -1,3 +1,58 @@
+## TERMUX package manager detection flow
+
+runsilent() {
+  {
+    {
+      eval "$*" # only meant to be used as one command
+    } &>/dev/null && return 0
+  } || return 1
+}
+
+pkgcheck() {
+
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    printf "
+${BLUE}Termux package manager detection${NC}
+    ${RED}Caution: do not use with && or || chains, as it always returns an error.${NC}
+  
+  ${WHITE}Scripting usage${NC}:
+
+    Capture \$?
+
+    if return code is ${BLUE}1${NC}, usable package manager is ${WHITE}pacman${NC}
+    if return code is ${BLUE}2${NC}, usable package manager is ${WHTIE}apt${NC}
+
+    if return code is ${RED}3${NC}, there are no usable package managers.
+    if return code is ${RED}4${NC}, both apt & pacman exist on the termux install.
+
+
+    ${BRIGHT_WHITE}ACT ACCORDINGLY, it is up to the PARENT script to handle these.
+    \n"
+    return
+    fi
+
+
+    aptstate=false
+    pacmanstate=false
+
+    runsilent apt --version && aptstate=true
+    runsilent pacman -V && pacmanstate=true
+
+    if [[ "$aptstate" == "true" ]]; then
+      if [[ "$pacmanstate" == "true" ]]; then
+        return 4
+      else
+        return 2
+      fi
+    else
+      if [[ "$pacmanstate" == "true" ]]; then
+        return 1
+      else
+        return 3
+      fi
+    fi
+}
+
 trap '' INT
 
 if [[ ! -f ~/.assets/pkg ]]; then
@@ -12,8 +67,9 @@ alias repkg='rm ~/.assets/pkg && szsh'
 alias szsh='clear && exec zsh'
 
 if [[ ! -s ~/.assets/pkg ]]; then
-      if [[ -f $PREFIX/bin/apt && -f $PREFIX/bin/pacman ]]; then
-        echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+  pkgcheck
+  case "$?" in
+    4) echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
         err "Unable to set package manager as you have two (apt + pacman).\n"
         info "Choose your primary [ apt / pacman ]: "
         local zzz
@@ -41,29 +97,29 @@ if [[ ! -s ~/.assets/pkg ]]; then
              repkg
              ;;
          esac
-      fi
+         ;;
 
-      if [[ ! -f $PREFIX/bin/apt && ! -f $PREFIX/bin/pacman ]]; then
-        err "Unable to set package manager as no supported were detected.\n"
+       3) err "Unable to set package manager as no supported were detected.\n"
         rm ~/.assets/pkg
         return
-      fi
+        ;;
 
-      if [[ -f $PREFIX/bin/apt ]]; then
+      2)
         __echoToPkg apt
         pkg="apt"
         clear
         bc_info "APT was set as the primary package manager.\n"
         return 0
-      fi
+        ;;
 
-      if [[ -f $PREFIX/bin/pacman ]]; then
+      1)
         __echoToPkg pacman
         pkg="pacman"
         clear
         bc_info "PACMAN was set as the primary package manager.\n"
         return 0
-      fi
+        ;;
+  esac
 fi
 
 filevalid="$(cat ~/.assets/pkg)"
